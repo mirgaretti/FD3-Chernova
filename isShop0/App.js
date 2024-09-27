4009aed2ecf9177bd608d58f777bbc15d9be7744
Margarita Chernova <margarita.p.chernova@gmail.com>
2024-09-27 13:37:27 +0300

    ishop0 task commit

---
+++ isShop0/App.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Shop from './components/Shop';
+
+ReactDOM.render(
+  <Shop name="Wildberries" address="ул. Коржа, 20"/>,
+  document.getElementById('container') 
+);
---
+++ isShop0/components/Shop.js
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+
+import './Shop.css';
+
+class Shop extends React.Component {
+
+  render() {
+    return (
+      <div className='Shop'>
+        <p className='Shop_Name'>
+          {this.props.name}
+        </p>
+        <p className='Shop_Address'>
+          {this.props.address}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Shop;
