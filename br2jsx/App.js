acaddd22b3dcf5495f6eb89e3317822d34a21484
Margarita Chernova <margarita.p.chernova@gmail.com>
2024-10-23 12:37:11 +0300

    br 2 jsx commit

---
+++ br2jsx/App.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import BR2JSX from './components/BR2JSX';
+
+class App extends React.Component {
+  render() {
+    let text = "первый<br>второй<br/>третий<br />последний";
+    return <BR2JSX text={text} />;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
---
+++ br2jsx/components/BR2JSX.js
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import './BR2JSX.css';	
+
+const BR2JSX = ({ text }) => {
+  const lines = text.split(/<br\s*\/?>/i); 
+  console.log(lines);
+
+  return (
+    <div className='BR2JSX'>
+      {
+        lines.reduce((acc, line, index) => {
+          acc.push(line); 
+          if (index < lines.length - 1) acc.push(<br key={index} />); 
+          return acc;
+        }, [])
+      }
+    </div>
+  );
+};
+
+export default BR2JSX;
