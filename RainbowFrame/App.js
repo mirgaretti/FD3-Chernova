25187e114a4096a3bc032c3221440b87906083f6
Margarita Chernova <margarita.p.chernova@gmail.com>
2024-10-23 12:35:28 +0300

    rainbow rame commit

---
+++ RainbowFrame/App.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import RainbowFrame from './components/RainbowFrame';
+
+class App extends React.Component {
+  render() {
+    let colors = ['#ed8805', '#566d05', '#b08152', '#893e0f', '#516a30', '#dc6c74', '#9c0424', '#eb8c50'];
+    return (
+      <RainbowFrame colors={colors}>
+        Hello!
+      </RainbowFrame>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import './RainbowFrame.css';
+
+const RainbowFrame = ({ colors, children }) => {
+  let content = children;
+
+  for (let i = colors.length - 1; i >= 0; i--) {
+    content = (
+      <div style={{ border: `10px solid ${colors[i]}`, padding: '10px' }} key={i}>
+        {content}
+      </div>
+    );
+  }
+
+  return <div>{content}</div>; 
+};
+
+export default RainbowFrame;
